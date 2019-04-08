0f15414d71ec1c422c1cecb7a77a60b41daa53ad
alex <you@example.com>
2019-04-08 21:42:19 +0000

    produced each avatar url thru for loop

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,24 @@
 var request = require('request');
+var getSecrets = require('./secrets')
 
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
-      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-      headers: {
-        'User-Agent' : 'request',
-        'Authorization' : 'ea8ea57512ae3db4f06e5c2b1f0d71382b468164'
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName +  "/contributors",
+       headers: {
+        'User-Agent' : 'request'
       }
     };
-  
+
     request(options, function(err, res, body) {
-      cb(err, body);
-    });
+      var data = JSON.parse(body)
+
+      cb(err, data);
+    }).auth('at0082a', getSecrets.GITHUB_TOKEN);
   }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
+    for (i = 0; i < result.length; i++) {
+        console.log(result[i].avatar_url);
+    }
     console.log("Errors:", err);
-    console.log("Result:", result);
  });
