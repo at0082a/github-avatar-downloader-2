55508fac45ac299a1d983f62527c798a1228f8fd
alex <you@example.com>
2019-04-08 22:21:41 +0000

    stored picture locally

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,7 @@
 var request = require('request');
-var getSecrets = require('./secrets')
+var getSecrets = require('./secrets');
+var fs = require('fs');
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
@@ -16,9 +18,25 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }).auth('at0082a', getSecrets.GITHUB_TOKEN);
   }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-    for (i = 0; i < result.length; i++) {
-        console.log(result[i].avatar_url);
-    }
-    console.log("Errors:", err);
- });
+  function downloadImageByURL(url, filePath) {
+    request.get(url)               
+       .on('error', function (err) {                                   
+         throw err; 
+       })
+       .on('response', function (response) {                           
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Message: ', response.statusMessage);
+         console.log('Response Headers: ', response.headers['content-type'])
+         console.log('Download complete.')
+       })
+       .pipe(fs.createWriteStream(filePath)); 
+  }
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//     for (i = 0; i < result.length; i++) {
+//         console.log(result[i].avatar_url);
+//     }
+//     console.log("Errors:", err);
+//  });
+
+ downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./photos/kvirani.jpg")
